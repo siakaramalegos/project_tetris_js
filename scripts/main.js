557ab928e11b0d08ea982e8b7ddd76e38f095569
Sia <siakaramalegos@gmail.com>
2016-05-23 20:23:18 -0500

    Initializes and renders board

--- scripts/main.js
+++ scripts/main.js
@@ -8,9 +8,52 @@ TET.MainModule = (function(){
   // Stub for returned object (public methods)
   var stub = {};
 
-  stub.init = function(){
+  var _rows = 20;
+  var _columns = 10;
+  var _board = [];
+  var _gameLoop;
 
-  }
+  stub.init = function(){
+    // Initialize game objects
+    _initBoard();
+    _renderBoard();
+
+    // Set listeners
+
+    // Start game loop
+    _gameLoop = window.setInterval(_tic, 2000);
+  };
+
+  var _initBoard = function(){
+    for (var i = 0; i < _columns; i++ ){
+      var row = [];
+      for (var j = 0; j < _rows; j++ ){
+        row.push(0);
+      }
+      _board.push(row);
+    }
+  };
+
+  var _tic = function(){
+    // Render objects
+    _renderBoard();
+
+    // Check game over
+  };
+
+  var _renderBoard = function(){
+    $('#game-board').html('');
+
+    for (var row = 0; row < _rows; row++) {
+      for (var col = 0; col < _columns; col++) {
+        var $square = $('<div></div>')
+          .attr('class', 'grid-box');
+
+        $('#game-board').append($square);
+      }
+      $('#game-board').append('<br>');
+    }
+  };
 
   // Return public methods
   return stub;
