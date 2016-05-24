b2c593b4f849cc4225cbace01d77721734f9ce84
Sia <siakaramalegos@gmail.com>
2016-05-23 19:51:58 -0500

    Basic app structure

---
+++ scripts/main.js
@@ -0,0 +1,17 @@
+'use strict;'
+
+// Namespace the game
+var TET = TET || {};
+
+TET.MainModule = (function(){
+
+  // Stub for returned object (public methods)
+  var stub = {};
+
+  stub.init = function(){
+
+  }
+
+  // Return public methods
+  return stub;
+})();
